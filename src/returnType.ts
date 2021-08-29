b1f972ad6cc69a6632a5d668319e7e76779cdabd
Shamaei.Kiarash <kiarash.shamaii@gmail.com>
2021-08-29 18:54:57 +0430

    end of basic

--- src/returnType.ts
+++ src/returnType.ts
@@ -3,3 +3,10 @@
 function addRetrun(n1: number, n2: number): number {
     return n1 + n2;
   }
+
+//if dont define return we must write this 
+ //const addwithAlias = (n1 : combine , n2 : combine)=>{
+//     if(typeof n1 === "number" && typeof n2 === "number")
+//     return n1 + n2 
+//     return n1.toString() +  n2.toString()
+// }
